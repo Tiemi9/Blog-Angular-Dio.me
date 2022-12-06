ddaacbbc00a5945b1be0e1f6324e710e01525fd5
Tiemi9 <cristina_tiemi.social@ayahoo.com>
2022-12-06 12:37:04 -0300

    making main card dinamic

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-card.component.css']
 })
 export class MainCardComponent implements OnInit {
+  photoCover:string = "https://images.pexels.com/photos/6190327/pexels-photo-6190327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  cardDate:Date = new Date("December 5, 2022")
+  cardTitle:string = "Afinal, o que é o Angular e para que serve?"
+  cardText:string = "O Angular é um framework JavaScript que simplifica não apenas a construção da interface de usuário, mas também o desenvolvimento de aplicações client-side diferenciadas, sejam elas para a web, mobile ou desktop. O Angular usa a linguagem TypeScript como padrão, ao invés do JavaScript. Então recomenda-se aprofundar os estudos sobre essa linguagem."
 
   constructor() { }
 
