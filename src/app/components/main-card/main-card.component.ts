49e3a68bd7e6e9d3f647b6eac379e3bd69688f98
Tiemi9 <cristina_tiemi.social@ayahoo.com>
2022-12-06 20:33:19 -0300

    Making Small Cards Dinamic

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -10,6 +10,7 @@ export class MainCardComponent implements OnInit {
   cardDate:Date = new Date("December 5, 2022")
   cardTitle:string = "Afinal, o que é o Angular e para que serve?"
   cardText:string = "O Angular é um framework JavaScript que simplifica não apenas a construção da interface de usuário, mas também o desenvolvimento de aplicações client-side diferenciadas, sejam elas para a web, mobile ou desktop. O Angular usa a linguagem TypeScript como padrão, ao invés do JavaScript. Então recomenda-se aprofundar os estudos sobre essa linguagem."
+  cardLink:string = "https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Angular_getting_started"
 
   constructor() { }
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-
+  smallCardPhotoCover: string = "https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  smallCardPhotoAlt: string = "Imagem de tela de computador com código desfocado"
+  smallCardDate: Date = new Date("December 5, 2022")
+  smallCardDiscription: string = "Angular CLI é uma ferramenta de linha de comando que facilita a criação de diversos artefatos necessários em uma aplicação Angular."
+  smallCardLink: string = "https://angular.io/cli"
   constructor() { }
 
   ngOnInit(): void {
