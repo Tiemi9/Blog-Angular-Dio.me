43ba1f0a125cce931b5cd54d39699e05e0820663
Tiemi9 <cristina_tiemi.social@ayahoo.com>
2022-12-06 21:18:12 -0300

    Add Input()

--- src/app/components/main-card/main-card.component.ts
+++ src/app/components/main-card/main-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
@@ -6,11 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-card.component.css']
 })
 export class MainCardComponent implements OnInit {
-  photoCover:string = "https://images.pexels.com/photos/6190327/pexels-photo-6190327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  cardDate:Date = new Date("December 5, 2022")
-  cardTitle:string = "Afinal, o que é o Angular e para que serve?"
-  cardText:string = "O Angular é um framework JavaScript que simplifica não apenas a construção da interface de usuário, mas também o desenvolvimento de aplicações client-side diferenciadas, sejam elas para a web, mobile ou desktop. O Angular usa a linguagem TypeScript como padrão, ao invés do JavaScript. Então recomenda-se aprofundar os estudos sobre essa linguagem."
-  cardLink:string = "https://developer.mozilla.org/en-US/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/Angular_getting_started"
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardDate:Date = new Date()
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardText:string = ""
+  @Input()
+  cardLink:string = ""
+  @Input()
+  linkTitle:string = ""
 
   constructor() { }
 
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -6,11 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./small-card.component.css']
 })
 export class SmallCardComponent implements OnInit {
-  smallCardPhotoCover: string = "https://images.pexels.com/photos/97077/pexels-photo-97077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-  smallCardPhotoAlt: string = "Imagem de tela de computador com código desfocado"
+  @Input()
+  smallCardPhotoCover: string = ""
+  @Input()
+  smallCardPhotoAlt: string = ""
+  @Input()
   smallCardDate: Date = new Date("December 5, 2022")
-  smallCardDiscription: string = "Angular CLI é uma ferramenta de linha de comando que facilita a criação de diversos artefatos necessários em uma aplicação Angular."
-  smallCardLink: string = "https://angular.io/cli"
+  @Input()
+  smallCardDiscription: string = ""
+  @Input()
+  smallCardLink: string = ""
   constructor() { }
 
   ngOnInit(): void {
